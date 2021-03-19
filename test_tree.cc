6a83350a6f56f831230d7c206f1482339324d271
Lucas Hamilton <lucas@macobserver.com>
2021-03-18 18:13:37 -0700

    Check that the path_to function does find the leftmost occurence of the key, even when a shorter right path exists

--- test_tree.cc
+++ test_tree.cc
@@ -42,25 +42,30 @@ tree_ptr_t add_child(tree_ptr_t parent, tree_ptr_t child, char direction)
 }
 int main()
 {
-	tree_ptr_t one = create_tree(4,3);
-	tree_ptr_t two = create_tree(3,3);
-	tree_ptr_t three = create_tree(2,1);
+	tree_ptr_t tree_one = create_tree(4,3);
+	tree_ptr_t tree_two = create_tree(3,3);
+	tree_ptr_t tree_three = create_tree(2,1);
+	tree_ptr_t tree_four = create_tree(5, 6);
+	tree_ptr_t tree_five = create_tree(7, 7);
+	tree_ptr_t tree_six = create_tree(2, 3);
 
-	tree_ptr_t four = add_child(one, two, 'L');
-	tree_ptr_t five = add_child(one, three, 'R');
+	tree_ptr_t four = add_child(tree_one, tree_two, 'L');
+	tree_ptr_t five = add_child(tree_one, tree_three, 'R');
+	tree_ptr_t six = add_child(tree_two, tree_four, 'R');
+	tree_ptr_t seven = add_child(tree_four, tree_six, 'L');
+	tree_ptr_t eight = add_child(tree_three, tree_five, 'L');
 
-	std::string check1 = path_to(one, 2);
-	char check1let = check1[0];
+	std::string check1 = path_to(tree_one, 2);
 	
-	tree_ptr_t check2 = add_child(one, three, 'L');
+	tree_ptr_t check2 = add_child(tree_one, tree_three, 'L');
 
-	assert(two == four);
-	assert(three == five);
+	assert(tree_two == four);
+	assert(tree_three == five);
 	std::cout<<"add_child works as expected"<<"\n";
 
-	char compareTo = 'R';
-	assert(check1let == compareTo);
-	std::cout<<"path_to works on a simple tree with no repeated values"<<"\n";
+	std::string compareTo = "LRL"; 
+	assert(check1 == compareTo);
+	std::cout<<"Finds the leftmost element, even when there is a closer one in the rightmost branch"<<"\n";
 
 	assert(check2 == nullptr);
 	std::cout<<"trying to add a child to a position where there is already a child yields nullptr"<<"\n";
