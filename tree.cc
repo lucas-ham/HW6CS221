e32e51234bdb031f1351ed8f8f4fd8256f3f52fb
Lucas Hamilton <lucas@macobserver.com>
2021-03-19 14:40:17 -0700

    Made the block comments into actual block comments instead of many single line comments of code

--- tree.cc
+++ tree.cc
@@ -27,11 +27,12 @@ void destroy_tree(tree_ptr_t tree)
 		destroy_tree(tree->right_);
 		delete tree;
 	}
-	//destroys/clears the memory space of the tree AND it's children
-	//easier to do this recursively?
-		//base case, current pointer is void, done
-		//otherwise delete node and run on all children
-		//this is the way to do it, roughly, just need to make sure commands make sense
+	/* destroys/clears the memory space of the tree AND it's children
+	easier to do this recursively?
+		base case, current pointer is void, done
+		otherwise delete node and run on all children
+		this is the way to do it, roughly, just need to make sure commands make sense
+		*/
 }
 
 std::string helper_path_to(tree_ptr_t tree, key_t key, std::string str);
@@ -39,7 +40,6 @@ std::string helper_path_to(tree_ptr_t tree, key_t key, std::string str);
 std::string path_to(tree_ptr_t tree, key_t key)
 {
 	//need to check every possible path, until the first instance of key is found
-	//recursion is your friend here too
 	auto str = "";
 	return helper_path_to(tree, key, str);
 }
@@ -109,11 +109,13 @@ tree_ptr_t node_at(tree_ptr_t tree, std::string path)
 			}
 		}
 	}
-	//follow a path from a node to the end of the given path
-	//not entirely sure what to do on this, need to somehow remove one char from path each time
-	//could do it with cases
-	//check path isn't empty, if it is return current tree
-	//seperate string into first char and rest of the string
-	//then switch letter, if right or left check that isn't null and call it again, 
-	//if not right or left return nullptr
+	/*
+	follow a path from a node to the end of the given path
+	not entirely sure what to do on this, need to somehow remove one char from path each time
+	could do it with cases
+	check path isn't empty, if it is return current tree
+	seperate string into first char and rest of the string
+	then switch letter, if right or left check that isn't null and call it again, 
+	if not right or left return nullptr
+	*/
 }
